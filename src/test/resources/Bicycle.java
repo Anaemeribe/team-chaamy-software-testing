c2f63e527f5ea6d094b3cbd8bbf03f90c6f0c6d7
fuaad001 <husseinfuaad@hotmail.com>
2023-04-25 22:35:19 -0400

    Add tests

--- src/test/resources/Bicycle.java
+++ src/test/resources/Bicycle.java
@@ -3,6 +3,11 @@ class Bicycle {
     // state or field
     private int gear = 5;
 
+    public Bicycle()
+    {
+
+    }
+
     // behavior or method
     public void braking() {
         System.out.println("Working of Braking");
